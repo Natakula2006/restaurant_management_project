50c6774764bab721eda159f86c542f1d72d2a4cb
Natakula2006 <natakulaharshini@gmail.com>
2025-08-07 18:41:11 +0530

    21191

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
     path('api/',include('products.urls')),
+    path('',include('home.urls')),
 ]
