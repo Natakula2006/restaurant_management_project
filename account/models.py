92ecceae8d980840313639b1384ba429936c7572
Natakula2006 <natakulaharshini@gmail.com>
2025-08-07 22:31:21 +0530

    21190

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE,related_name = 'profile')
+    phone_number = models.CharField(max_length = 15)
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
