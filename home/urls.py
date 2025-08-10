b2a4cc9cab81bb9e32fb7cd657f606eaaf8b4c66
Natakula2006 <natakulaharshini@gmail.com>
2025-08-10 16:17:06 +0530

    26961

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',menu_home,name='menu_home'),
     path('api/menu/',MenuView.as_view(),name='menu'),
     path('about/',views.about,name='about'),
+    path('contact/',views.contact_view,name='contact'),
     
 ]
