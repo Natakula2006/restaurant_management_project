0214e30f3b1acbaf0d90978da9202d671b9264d8
Natakula2006 <natakulaharshini@gmail.com>
2025-08-12 18:49:47 +0530

    28896

--- home/views.py
+++ home/views.py
@@ -1,14 +1,29 @@
 from django.contrib import settings
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    context = {'restaurant_name':getattr(settings,'RESTAURANT_NAME','My Restaurant'),'phone-number':getattr(settings,'RESTAURANT_PHONE_NUMBER','Not available')}
-    return render(request,'home.html',context)
+    try:
+    
+        context = {'restaurant_name':getattr(settings,'RESTAURANT_NAME','My Restaurant'),'phone-number':getattr(settings,'RESTAURANT_PHONE_NUMBER','Not available')}
+        return render(request,'home.html',context)
+    except Exception as e:
+        return HttpResponse(f"An error occured: {e}",status= 500)
 def about(request):
-    return render(request,'about.html')
+    try:
+        return render(request,'about.html')
+    except Exception as e:
+        return HttpResponse(f"An error occured: {e}",status = 500)
+
 def contact_view(request):
-    request render(request,'contact.html')
+    try:
+        return render(request,'contact.html')
+    except Exception as e:
+        return HttpResponse(f"An error occured: {e}",status = 500)
 def reservations(request):
-    request render(request,'reservations.html')
+    try:
+        return render(request,'reservations.html')
+    except Exception as e:
+        return HttpResponse(f"An error occured: {e}",status= 500)
 
